6175bb24fe75da9cf08ef5712c7d91540865c7ad
Kawamura Shintaro <kawasin73@gmail.com>
2017-07-03 18:41:11 +0900

    Show ListView

--- app/src/main/java/com/kawasin73/webrtcphonesample/MainActivity.java
+++ app/src/main/java/com/kawasin73/webrtcphonesample/MainActivity.java
@@ -1,9 +1,21 @@
 package com.kawasin73.webrtcphonesample;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.skyway.Peer.Browser.Navigator;
 import io.skyway.Peer.OnCallback;
@@ -15,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private Peer peer;
     private String currentId;
     private TextView idTextView;
+    private ListView listView;
+    private MyAdapter adapter;
+    private List<String> idList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
         Navigator.initialize(peer);
 
         idTextView = (TextView) findViewById(R.id.id_textview);
+        listView = (ListView) findViewById(R.id.listview);
+
+        adapter = new MyAdapter(this, 0, idList);
+        listView.setAdapter(adapter);
+
+        idList.clear();
+        idList.add("Hello1");
+        idList.add("Hello2");
+        idList.add("Hello3");
+        idList.add("Hello4");
+        adapter.notifyDataSetChanged();
 
         showCurrentPeerId();
     }
@@ -49,5 +75,24 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private class MyAdapter extends ArrayAdapter<String> {
+        private LayoutInflater inflater;
+
+        MyAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<String> objects) {
+            super(context, resource, objects);
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            View view = inflater.inflate(R.layout.list_item, null, false);
+            TextView textView = (TextView) view.findViewById(R.id.item_textview);
+            String name = getItem(position);
+            textView.setText(name);
+            return view;
+        }
     }
 }
