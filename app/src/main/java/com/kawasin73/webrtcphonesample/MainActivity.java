087113c4020a85e623d5362eb1c27cbe21048686
Kawamura Shintaro <kawasin73@gmail.com>
2017-07-03 18:11:02 +0900

    Show currentPeerId

--- app/src/main/java/com/kawasin73/webrtcphonesample/MainActivity.java
+++ app/src/main/java/com/kawasin73/webrtcphonesample/MainActivity.java
@@ -2,12 +2,52 @@ package com.kawasin73.webrtcphonesample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import io.skyway.Peer.Browser.Navigator;
+import io.skyway.Peer.OnCallback;
+import io.skyway.Peer.Peer;
+import io.skyway.Peer.PeerOption;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = getClass().getSimpleName();
+    private Peer peer;
+    private String currentId;
+    private TextView idTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PeerOption options = new PeerOption();
+        options.key = BuildConfig.SKYWAY_API_KEY;
+        options.domain = BuildConfig.SKYWAY_HOST;
+        peer = new Peer(this, options);
+        Navigator.initialize(peer);
+
+        idTextView = (TextView) findViewById(R.id.id_textview);
+
+        showCurrentPeerId();
+    }
+
+    private void showCurrentPeerId() {
+        peer.on(Peer.PeerEventEnum.OPEN, new OnCallback() {
+            @Override
+            public void onCallback(Object o) {
+                if (o instanceof String) {
+                    currentId = (String) o;
+                    Log.d(TAG, "currentId: " + currentId);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            idTextView.setText("ID: " + currentId);
+                        }
+                    });
+                }
+            }
+        });
     }
 }
