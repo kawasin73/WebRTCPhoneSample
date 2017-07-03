52dc8f51529c23bb3e155d97a277c75bd89fe086
Kawamura Shintaro <kawasin73@gmail.com>
2017-07-03 19:24:37 +0900

    Reqeust Permission

--- app/src/main/java/com/kawasin73/webrtcphonesample/MainActivity.java
+++ app/src/main/java/com/kawasin73/webrtcphonesample/MainActivity.java
@@ -1,9 +1,13 @@
 package com.kawasin73.webrtcphonesample;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -27,6 +31,7 @@ import io.skyway.Peer.Peer;
 import io.skyway.Peer.PeerOption;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int RECORD_AUDIO_REQUEST_ID = 1;
     private String TAG = getClass().getSimpleName();
     private Peer peer;
     private String currentId;
@@ -39,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkAudioPermission();
 
         PeerOption options = new PeerOption();
         options.key = BuildConfig.SKYWAY_API_KEY;
@@ -63,6 +70,52 @@ public class MainActivity extends AppCompatActivity {
         showCurrentPeerId();
 
         refreshPeerList();
+    }
+
+    private void checkAudioPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Manifest.permission.RECORD_AUDIO is not GRANTED");
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                Log.d(TAG, "shouldShowRequestPermissionRationale = false");
+                // Show an expanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                Log.d(TAG, "request Permission RECORD_AUDIO");
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        RECORD_AUDIO_REQUEST_ID);
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case RECORD_AUDIO_REQUEST_ID: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d(TAG, "request Permission RECORD_AUDIO GRANTED!");
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    Log.d(TAG, "request Permission RECORD_AUDIO DENIED!");
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     private void showCurrentPeerId() {
